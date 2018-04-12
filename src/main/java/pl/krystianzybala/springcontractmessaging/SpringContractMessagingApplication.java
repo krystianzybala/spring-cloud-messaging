2e54fbb0e487417ba80c153bf479d3be33227832
Krystian Zybala <kontakt@kzybala.pl>
2018-04-12 17:41:58 +0200

    base application

---
+++ src/main/java/pl/krystianzybala/springcontractmessaging/SpringContractMessagingApplication.java
@@ -0,0 +1,12 @@
+package pl.krystianzybala.springcontractmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContractMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringContractMessagingApplication.class, args);
+    }
+}
