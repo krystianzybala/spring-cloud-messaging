0419a119be7b4013d775aa8749c93517ad8f2c72
Krystian Zybala <kontakt@kzybala.pl>
2018-04-12 18:03:40 +0200

    Add test which verifies all communications is good

--- src/test/java/pl/krystianzybala/springcontractmessaging/SpringContractMessagingApplicationTests.java
+++ src/test/java/pl/krystianzybala/springcontractmessaging/SpringContractMessagingApplicationTests.java
@@ -1,16 +1,31 @@
 package pl.krystianzybala.springcontractmessaging;
 
+import org.assertj.core.api.BDDAssertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.StubTrigger;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureStubRunner(ids = "pl.krystianzybala:contract", stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 public class SpringContractMessagingApplicationTests {
 
+    @Autowired
+    private FraudListener fraudListener;
+
+    @Autowired
+    private StubTrigger stubTrigger;
+
     @Test
-    public void contextLoads() {
+    public void testConsumeMessage() {
+
+        stubTrigger.trigger("triggerFraud");
+        BDDAssertions.then(this.fraudListener.message).isEqualTo("Krystian send message!");
     }
 
 }
