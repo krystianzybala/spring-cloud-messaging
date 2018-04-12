0834a79ca10595270b3701d676ec0d8ab6f4f4e3
Krystian Zybala <kontakt@kzybala.pl>
2018-04-12 18:03:22 +0200

    Add listener and bindings for sink class

--- src/main/java/pl/krystianzybala/springcontractmessaging/SpringContractMessagingApplication.java
+++ src/main/java/pl/krystianzybala/springcontractmessaging/SpringContractMessagingApplication.java
@@ -1,12 +1,45 @@
 package pl.krystianzybala.springcontractmessaging;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class SpringContractMessagingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringContractMessagingApplication.class, args);
     }
+}
+
+@Component
+@Slf4j
+class FraudListener {
+    String message;
+
+    @StreamListener(Sink.INPUT)
+    public void onFraudMessage(FraudMessage  fraud) {
+
+        log.info("Message: {}", fraud.getMessage());
+
+        this.message = fraud.getMessage();
+    }
+}
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+class FraudMessage {
+    private String message;
 }
